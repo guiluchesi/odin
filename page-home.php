f554518e5f4ba3218007c0ba390e236022fa8b23
Guilherme Luchesi <gui.olhenrique@gmail.com>
2016-07-29 11:37:41 -0300

    troca slider por banner

--- page-home.php
+++ page-home.php
@@ -15,37 +15,11 @@ get_header();
 		<?php
 			// Start the Loop.
 			while ( have_posts() ) : the_post();
-				$imagens = get_field('slider'); ?>
+				$imagemBanner = get_field('banner_principal_img'); ?>
 
-				<div id="home-carousel" class="carousel slide" data-ride="carousel">
-					<ol class="carousel-indicators">
-						<?php
-							foreach ($imagens as $index => $imagem) {
-								$imgUrl = $imagem['url'];
-						?>
-							<li data-target="#home-carousel" data-slide-to="<?php echo $index;?>" <?php if($index === 0){echo 'class="active"';}?>></li>
-						<?php } ?>
-
-					</ol>
-					<div class="carousel-inner" role="listbox">
-						<?php
-							foreach ($imagens as $index => $imagem) {
-								$imgUrl = $imagem['url'];
-						?>
-							<div class="item <?php if($index === 0){echo 'active';}?>">
-								<img src="<?php echo $imgUrl; ?>" alt="First slide">
-							</div>
-						<?php } ?>
-					</div>
-					<a class="left carousel-control" href="#home-carousel" role="button" data-slide="prev">
-						<span class="icon-prev" aria-hidden="true"></span>
-						<span class="sr-only">Previous</span>
-					</a>
-					<a class="right carousel-control" href="#home-carousel" role="button" data-slide="next">
-						<span class="icon-next" aria-hidden="true"></span>
-						<span class="sr-only">Next</span>
-					</a>
-				</div>
+			<div class="banner-principal">
+				<img src="<?php echo $imagemBanner[url]; ?>" alt="" class="img-rounded center-block">
+			</div>
 
 			<div id="wrapper" class="container">
 				<div class="row">
